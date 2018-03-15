9f4d5b4fd39f2a285c23e5ce371448d733076413
juan sebastian cruz torrico <jsebas.ct@gmail.com>
2018-03-15 17:21:59 -0300

    chore: project creation

---
+++ src/main/java/org/demo/people/creditcard/CreditcardApplication.java
@@ -0,0 +1,12 @@
+package org.demo.people.creditcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardApplication.class, args);
+	}
+}
